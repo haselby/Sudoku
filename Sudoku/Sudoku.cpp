fb66fb2acdc6a44ddc6ee36403b79864c5244ca4
haselby <jeffrey.haselby@gmail.com>
2021-01-23 17:03:40 -0700

    Completed Recursive Backtracking Solver, not inforcing internal grids

--- Sudoku/Sudoku.cpp
+++ Sudoku/Sudoku.cpp
@@ -4,12 +4,13 @@
 #include <iostream>
 #include <boost/multi_array.hpp>
 #include <string>
+#include <tuple>
 
 //type definiton for 2D matrix using boost library
 typedef boost::multi_array<int, 2> grid_type;
 typedef grid_type::index index;
 
-void prettyPrintPuzzle(grid_type myPuzzle) {
+void PrettyPrintPuzzle(grid_type myPuzzle) {
 
     for (index i = 0; i != 9; ++i) {
         for (index j = 0; j != 9; ++j) {
@@ -18,10 +19,12 @@ void prettyPrintPuzzle(grid_type myPuzzle) {
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
 
 }
 
-void printPuzzle(grid_type myPuzzle){
+
+void PrintPuzzle(grid_type myPuzzle){
 
     for (index i = 0; i != 9; ++i){
         for (index j = 0; j != 9; ++j) {
@@ -29,34 +32,109 @@ void printPuzzle(grid_type myPuzzle){
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
+
+}
+
+std::string DefinePuzzle(){
+    //Define Sudoku Puzzle to solve
+//Use 0 to indicate empty cell
+    std::string puzzleRaw_{};
+    puzzleRaw_.append("400689200");
+    puzzleRaw_.append("380400600");
+    puzzleRaw_.append("201300800");
+    puzzleRaw_.append("008067032");
+    puzzleRaw_.append("056090080");
+    puzzleRaw_.append("030850096");
+    puzzleRaw_.append("600000175");
+    puzzleRaw_.append("004021008");
+    puzzleRaw_.append("807006000");
+
+    return puzzleRaw_;
+}
+
+grid_type LoadPuzzle() {
+    grid_type puzzleInitial(boost::extents[9][9]);
+    
+    std::string puzzleRaw = DefinePuzzle();
+
+    // Read in puzzle from puzzleRaw string
+    for (index i = 0; i != 9; ++i)
+        for (index j = 0; j != 9; ++j)
+            puzzleInitial[i][j] = std::stoi(puzzleRaw.substr(i * 9 + j, 1));
+    
+    return puzzleInitial;
+}
+
+bool PuzzleComplete(grid_type puzzle) {
+ 
+    for (index i = 0; i != 9; ++i)
+        for (index j = 0; j != 9; ++j)
+            if (puzzle[i][j] == 0) return false;
+    
+    return true;
+}
+
+std::tuple<int, int> NextOpenPosition(grid_type puzzle) {
+    for (index i = 0; i != 9; ++i)
+        for (index j = 0; j != 9; ++j)
+            if (puzzle[i][j] == 0) return std::make_tuple(i,j);
+}
+
+bool IsValidGuess(grid_type puzzle, int guess, int row, int column) {
+    
+    for (index i = 0; i != 9; ++i)
+        if (puzzle[i][column] == guess) return false;
+
+    for (index j = 0; j != 9; ++j)
+        if (puzzle[row][j] == guess) return false;
+
+    //TODO: Add sub matrix check
+
+    return true;
+    
+}
+
+bool Solve(grid_type puzzle) {
+    //Recursive Backtracking Solver
+    
+    //Base Case
+    if (PuzzleComplete(puzzle)) {
+        PrintPuzzle(puzzle);
+        return true;
+    }
+ 
+    //Standard Case
+
+    //identify row,column number of next open position
+    auto[row, column] = NextOpenPosition(puzzle);
+
+    for (int nextGuess = 1; nextGuess < 10; ++nextGuess) {
+        if (IsValidGuess(puzzle, nextGuess, row, column)) {
+            //try one choice i, if it works out, your are done.
+            //if (Solve(puzzle)) return true;
+            puzzle[row][column] = nextGuess;
+            if (Solve(puzzle)) return true;
+        }
+       
+        //otherwise unmake choice at the current level of recursion
+        puzzle[row][column] = 0;
+    }
+
+    return false; //tried all choices, no solution found
 
 }
 
 int main() {
 
-    //Define Sudoku Puzzle to solve
-    //Use 0 to indicate empty cell
-    std::string puzzleRaw{};
-    puzzleRaw.append("400689200");
-    puzzleRaw.append("380400600");
-    puzzleRaw.append("201300800");
-    puzzleRaw.append("008067032");
-    puzzleRaw.append("056090080");
-    puzzleRaw.append("030850096");
-    puzzleRaw.append("600000175");
-    puzzleRaw.append("004021008");
-    puzzleRaw.append("807006000");
-
     // Create a 2D array named puzzle that is 9 rows x 9 columns
     grid_type puzzle(boost::extents[9][9]);
 
-    // Read in puzzle from puzzleRaw string
-    
-    for (index i = 0; i != 9; ++i)
-        for (index j = 0; j != 9; ++j)
-            puzzle[i][j] = std::stoi(puzzleRaw.substr(i*9+j, 1));
+    puzzle = LoadPuzzle();
 
-    prettyPrintPuzzle(puzzle);
+    PrettyPrintPuzzle(puzzle);
+
+    Solve(puzzle);
 
     system("pause");
     return 0;
